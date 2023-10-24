bb6e39f5e8d4200c50172f325e938d887b5c2f27
hungerbk <wjantlr@gmail.com>
2023-10-24 21:30:26 +0900

    [학습] ECMA Script6 - Template literals

--- workspace/ch01/ex01-20.js
+++ workspace/ch01/ex01-20.js
@@ -1,8 +1,8 @@
 var user = {
-  userName: '김철수',
+  userName: "김철수",
   age: 30,
-  phone: '0102223333',
-  address: '서울특별시 종로구 종로3길 17 D타워, 16-17층 (주)멋쟁이사자처럼'
+  phone: "0102223333",
+  address: "서울특별시 종로구 종로3길 17 D타워, 16-17층 (주)멋쟁이사자처럼",
 };
 
 /* 다음처럼 출력할 수 있도록 info를 작성하시오.
@@ -11,7 +11,28 @@ var user = {
 전화번호: 0102223333
 주소: 서울시
 */
-var info = '';
+
+var info = user.userName + "님의 정보입니다.\n나이: " + user.age + "\n전화번호: " + user.phone + "\n주소: " + user.address;
+var info = `${user.userName}님의 정보입니다.
+나이: ${user.age}
+전화번호: ${user.phone}
+주소: ${user.address}`;
 
 console.log(info);
 
+// Tagged Template Literal
+// 텍스트만 따로 배열로 전달이 됨(변수 사이에 있는 문자열끼리 묶여서 배열로. 순서대로 인덱스 지정됨)
+function userInfo(strings, userName, age, phone, address) {
+  console.log(strings);
+  console.log(userName);
+  console.log(age);
+  console.log(phone);
+  console.log(address);
+}
+
+var kim = userInfo`
+  ${user.userName}님의 정보입니다.
+  나이: ${user.age}
+  전화번호: ${user.phone}
+  주소: ${user.address}
+`;
