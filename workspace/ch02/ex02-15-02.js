71c8e9b81ea23beee45fa141c6205e03899c041c
hungerbk <wjantlr@gmail.com>
2023-10-25 21:56:05 +0900

    [학습] 함수 호출과 this 내부의 this 바인딩 문제 해결 (call)

--- workspace/ch02/ex02-15-02.js
+++ workspace/ch02/ex02-15-02.js
@@ -1,1 +1,18 @@
 // ex02-15.js 복사
+var count = 0;
+var counter = {
+  count: 0,
+  visit: function () {
+    // 방문자를 한명 증가시킨다.
+    this.count++; // this = counter
+    var visit2 = function () {
+      this.count++; // this = counter
+    };
+    // 15-01처럼 함수 내부를 수정하지 않고 this 전달 가능
+    visit2.call(this);
+  },
+};
+
+counter.visit();
+counter.visit();
+console.log("합계", counter.count);
