519724849991a8f525baaadc1656f6d388e4dea7
hungerbk <wjantlr@gmail.com>
2023-10-25 21:03:12 +0900

    [학습] 함수 호출과 this - 메서드 호출시 this 바인딩

--- workspace/ch02/ex02-14.js
+++ workspace/ch02/ex02-14.js
@@ -1,8 +1,27 @@
-var name = 'global';
-function returnName(){
-	return this.name;
+var name = "global";
+function returnName() {
+  return this.name;
 }
 
 // 일반적인 함수로 호출(함수명())
+console.log(returnName()); //global
 
 // 객체의 메소드로 호출(객체.메소드명())
+var kim = new Object();
+kim.name = "김철수";
+kim.age = 30;
+kim.getName = returnName;
+
+var lee = {
+  name: "이영희",
+  age: 40,
+  getName: returnName,
+};
+
+console.log(kim.age, kim.getName());
+console.log(lee.age, lee.getName());
+
+// this call, apply
+console.log(kim.getName(), kim.getName.call(window));
+console.log(lee.getName(), lee.getName.apply(kim));
+console.log(returnName(), returnName.call(lee));
