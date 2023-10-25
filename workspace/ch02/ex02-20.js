4b414cd9fe70b9ae32d84a886c8d3005f7335a04
hungerbk <wjantlr@gmail.com>
2023-10-25 21:58:06 +0900

    [학습] 1025 과제 제출

--- workspace/ch02/ex02-20.js
+++ workspace/ch02/ex02-20.js
@@ -1,50 +1,89 @@
 var todolist = [
   {
-    "_id": 1,
-    "title": "Javascript 공부",
-    "done": true
+    _id: 1,
+    title: "Javascript 공부",
+    done: true,
   },
   {
-    "_id": 2,
-    "title": "Typescript 공부",
-    "done": false
+    _id: 2,
+    title: "Typescript 공부",
+    done: false,
   },
   {
-    "_id": 3,
-    "title": "React 공부",
-    "done": false
+    _id: 3,
+    title: "React 공부",
+    done: false,
   },
   {
-    "_id": 4,
-    "title": "React 프로젝트",
-    "done": true
-  }
-]
+    _id: 4,
+    title: "React 프로젝트",
+    done: true,
+  },
+];
+
+// 배열 메서드 사용하지 않고, for문이나 if문을 사용해서 작성하기
 
 // 완료된 할일 목록
 var doneList = [];
-console.log('완료된 할일 목록', doneList);
+for (let i = 0; i < todolist.length; i++) {
+  if (todolist[i].done === true) {
+    doneList.push(todolist[i]);
+  }
+}
+console.log("완료된 할일 목록", doneList);
 
 // 남은 할일 목록
 var reaminList = [];
-console.log('남은 할일 목록', reaminList);
+for (let i = 0; i < todolist.length; i++) {
+  if (todolist[i].done === false) {
+    reaminList.push(todolist[i]);
+  }
+}
+console.log("남은 할일 목록", reaminList);
 
 // 남은 할일 수
-var reaminCount = 0;
-console.log('남은 할일 수', reaminCount);
+var remainCount = 0;
+for (let i = 0; i < todolist.length; i++) {
+  if (todolist[i].done === false) {
+    remainCount++;
+  }
+}
+console.log("남은 할일 수", remainCount);
 
 // _id=2인 할일
 var todo = {};
-console.log('_id=2인 할일', todo);
+for (let i = 0; i < todolist.length; i++) {
+  if (todolist[i]["_id"] === 2) {
+    todo = todolist[i];
+  }
+}
+console.log("_id=2인 할일", todo);
 
 // _id=3인 할일의 index
 var todoIndex = 0;
-console.log('_id=3인 할일의 index', todoIndex);
+for (let i = 0; i < todolist.length; i++) {
+  if (todolist[i]["_id"] === 3) {
+    todoIndex = i;
+  }
+}
+console.log("_id=3인 할일의 index", todoIndex);
 
 // 남은 할일이 하나라도 있는가?
 var hasTodo = false;
-console.log('남은 할일이 하나라도 있는가?', hasTodo);
+for (let i = 0; i < todolist.length; i++) {
+  if (todolist[i].done === false) {
+    hasTodo = true;
+    break;
+  }
+}
+console.log("남은 할일이 하나라도 있는가?", hasTodo);
 
 // 할일이 모두 완료 되었는가?
 var busy = true;
-console.log('할일이 모두 완료 되었는가?', busy);
+for (let i = 0; i < todolist.length; i++) {
+  if (todolist[i].done === false) {
+    busy = false;
+    break;
+  }
+}
+console.log("할일이 모두 완료 되었는가?", busy);
