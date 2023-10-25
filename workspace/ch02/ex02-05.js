1e39f29198dc2cbf3971acf6b88ff6a85f91f95f
hungerbk <wjantlr@gmail.com>
2023-10-25 20:57:40 +0900

    [학습] 함수 생성 - 선언문

--- workspace/ch02/ex02-05.js
+++ workspace/ch02/ex02-05.js
@@ -1,1 +1,9 @@
 // 선언문 방식의 함수 생성
+function add(x, y) {
+  return x + y;
+}
+
+// this call, apply
+console.log(add(10, 20));
+console.log(add.call(this, 10, 20));
+console.log(add.apply(this, [10, 20]));
