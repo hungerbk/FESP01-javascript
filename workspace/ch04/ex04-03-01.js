9b527a68a93921665e6953594ee3dfd125e9a108
hungerbk <wjantlr@gmail.com>
2023-10-26 22:37:46 +0900

    [학습] 클로저 용법 - 커링 사용 사례

--- workspace/ch04/ex04-03-01.js
+++ workspace/ch04/ex04-03-01.js
@@ -2,4 +2,17 @@ function log(target, level, message) {
   const now = new Date();
   console.log(`[${now.getHours()}:${now.getMinutes()}] [${target}] [${level}] ${message}`);
 }
-log('console', 'DEBUG', 'debug message...');
+log("console", "DEBUG", "debug message...");
+log("console", "DEBUG", "debug message2...");
+log("console", "ERROR", "some error...");
+
+// 이렇게 여러개롤 쪼개면 함수의 재활용성이 높아짐
+var logger = _.curry(log);
+logger("console")("DEBUG")("hello");
+
+var myLog = logger("network");
+myLog.debug = myLog("DEBUG");
+myLog.info = myLog("INFO");
+
+myLog.info("server started on port 1234");
+myLog.debug("server test ... OK");
