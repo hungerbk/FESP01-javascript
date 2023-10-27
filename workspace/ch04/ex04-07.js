6e88cbee33a8352f07670760a52169cc0cfedc95
hungerbk <wjantlr@gmail.com>
2023-10-27 13:02:25 +0900

    [학습] 즉시실행 함수 - 즉시실행 함수

--- workspace/ch04/ex04-07.js
+++ workspace/ch04/ex04-07.js
@@ -1,4 +1,26 @@
-var alert = '출입 금지!';
-console.log(alert);
+// var alert = "출입 금지!"; // 전역에 alert이 있기 때문에 에러
+// console.log(alert);
 
-alert('작업 완료.');
+// alert이라는 변수는 안 만들었지만, fn이라는 기명 함수 생성(전역)
+function fn() {
+  var alert = "출입 금지!";
+  console.log(alert);
+}
+fn();
+
+(function () {
+  var alert = "출입 금지!";
+  console.log(alert);
+})();
+
+// 매개변수 전달
+function fn2(alert) {
+  console.log(alert);
+}
+alert("출입 금지!!");
+
+((alert) => {
+  console.log(alert);
+})("출입금지!!!");
+
+alert("작업 완료.");
