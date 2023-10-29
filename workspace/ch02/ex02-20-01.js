df1269006b6822ad7790b6c917993b2fd6ca8875
hungerbk <wjantlr@gmail.com>
2023-10-29 10:02:22 +0900

    과제 수정 - 조건문 수정

--- workspace/ch02/ex02-20-01.js
+++ workspace/ch02/ex02-20-01.js
@@ -24,22 +24,22 @@ var todolist = [
 // forEach() 완료된 할일 목록
 var doneList = [];
 todolist.forEach((elem) => {
-  if (elem.done === true) {
+  if (elem.done) {
     doneList.push(elem);
   }
 });
 console.log("forEach() 완료된 할일 목록", doneList);
 
 // filter() 완료된 할일 목록
-var doneList = todolist.filter((elem) => elem.done === true);
+var doneList = todolist.filter((elem) => elem.done);
 console.log("filter() 완료된 할일 목록", doneList);
 
 // map() 남은 할일 목록
-var reaminList = todolist.map((elem) => (elem.done ? undefined : elem.title)).filter((val) => val !== undefined);
+var reaminList = todolist.map((elem) => (elem.done ? null : elem)).filter((val) => val);
 console.log("map() 남은 할일 목록", reaminList);
 
 // reduce() 남은 할일 수
-var reaminCount = todolist.reduce((acc, cur) => (!cur.done ? acc + 1 : acc), 0);
+var reaminCount = todolist.reduce((acc, cur) => acc + (!cur.done ? 1 : 0), 0);
 console.log("reduce() 남은 할일 수", reaminCount);
 
 // find() _id=2인 할일
@@ -51,9 +51,9 @@ var todoIndex = todolist.indexOf(todolist.find((elem) => elem._id === 3));
 console.log("find() _id=3인 할일의 index", todoIndex);
 
 // some() 남은 할일이 하나라도 있는가?
-var hasTodo = todolist.some((elem) => elem.done === false);
+var hasTodo = todolist.some((elem) => !elem.done);
 console.log("some() 남은 할일이 하나라도 있는가?", hasTodo);
 
 // every() 할일이 모두 완료 되었는가?
-var busy = todolist.every((elem) => elem.done === true);
+var busy = todolist.every((elem) => elem.done);
 console.log("every() 할일이 모두 완료 되었는가?", busy);
